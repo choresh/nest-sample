23be2062f3f552be3bcf08bbb8b82ddf884215db
choresh <39343045+choresh@users.noreply.github.com>
2023-02-25 01:09:37 +0200

    Improve 'Prop' decorator.

--- src/infra/decorators/prop.ts
+++ src/infra/decorators/prop.ts
@@ -8,6 +8,9 @@ export interface OneToMany {
   foreignField: string
 }
 
+const PRIMARY_KEYS_NAME = '_id'
+const PRIMARY_KEYS_TYPE = ID
+
 /**
  * Decorator that marks a property as:
  * 1) A GraphQL property.
@@ -20,7 +23,7 @@ export function Prop (options: { nullable?: boolean } & { primaryKey?: boolean }
     const graphQlType = (options.type !== undefined)
       ? [options.type]
       : (options.primaryKey === true)
-          ? ID
+          ? PRIMARY_KEYS_TYPE
           : reflectedType
 
     Field(() => graphQlType, { nullable: options.nullable })(target, key) // Apply the graphql @Field() decorator to the property.
@@ -28,17 +31,18 @@ export function Prop (options: { nullable?: boolean } & { primaryKey?: boolean }
     if (options.primaryKey !== true) {
       const typegooseOptions: TypegooseOptions = {
       }
-      if (options.type !== undefined) {
-        typegooseOptions.ref = options.type
-        typegooseOptions.localField = '_id'
-        typegooseOptions.autopopulate = true
-        typegooseOptions.type = reflectedType
-      }
       if (options.required !== undefined) {
         typegooseOptions.required = options.required
       }
       if (options.oneToMany !== undefined) {
+        if (options.type === undefined) {
+          throw new Error(`Option 'type' is required, at '@Prop()' decorator, above property '${target.constructor.name as string}.${key}'`)
+        }
         typegooseOptions.foreignField = options.oneToMany.foreignField
+        typegooseOptions.ref = options.type
+        typegooseOptions.localField = PRIMARY_KEYS_NAME
+        typegooseOptions.autopopulate = true
+        typegooseOptions.type = reflectedType
       }
       TypegooseProp(typegooseOptions)(target, key) // Apply the typegoose @Prop() decorator to the property.
     }
