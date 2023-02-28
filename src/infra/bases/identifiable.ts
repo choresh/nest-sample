d3aafce313a213d43daa32c7f34f73c001fbf456
choresh <39343045+choresh@users.noreply.github.com>
2023-02-28 09:33:00 +0200

    Update identifiable.ts

--- src/infra/bases/identifiable.ts
+++ src/infra/bases/identifiable.ts
@@ -1,9 +1,9 @@
-import { mongoose } from '@typegoose/typegoose'
+import { ObjectId } from 'mongoose'
 import { Entity } from '../decorators/entity'
 import { Prop } from '../decorators/prop'
 
 @Entity()
 export class Identifiable {
   @Prop({ primaryKey: true })
-  readonly _id: mongoose.Types.ObjectId
+  readonly _id: ObjectId
 }
