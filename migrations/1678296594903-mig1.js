28e3f99198463af72da0ea1cc88b06739e205be6
choresh <39343045+choresh@users.noreply.github.com>
2023-03-08 19:32:58 +0200

    Improve migrations.

--- migrations/1678296594903-mig1.js
+++ migrations/1678296594903-mig1.js
@@ -1,14 +1,12 @@
-const moongoose = require('@typegoose/typegoose')
-const User = require('./users/entities/user.entity')
-console.log(User)
-const userModel = moongoose.getModelForClass(User)
-console.log(userModel)
+const typegoose = require('@typegoose/typegoose')
+const userEntity = require('./../dist/src/users/entities/user.entity')
+const userModel = typegoose.getModelForClass(userEntity.User)
 
 /**
  * Make any changes you need to make to the database here
  */
 async function up () {
-  await userModel.syncIndexes()
+  // Write migration here
   await userModel.updateMany({}, {
     $rename: { name: 'employeId' }
   }, { multi: true })
@@ -18,9 +16,7 @@ async function up () {
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down () {
-  await userModel.updateMany({}, {
-    $rename: { employeId: 'name' }
-  }, { multi: true })
+  // Write migration here
 }
 
 module.exports = { up, down }
