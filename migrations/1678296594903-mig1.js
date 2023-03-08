49549cad935d4b32c0cf1744cb2f8714bcc50910
choresh <39343045+choresh@users.noreply.github.com>
2023-03-08 20:32:35 +0200

    Update 1678296594903-mig1.js

--- migrations/1678296594903-mig1.js
+++ migrations/1678296594903-mig1.js
@@ -1,22 +1,14 @@
 const typegoose = require('@typegoose/typegoose')
-const userEntity = require('./../dist/src/users/entities/user.entity')
+const userEntity = require('./../dist/users/entities/user.entity')
 const userModel = typegoose.getModelForClass(userEntity.User)
 
-/**
- * Make any changes you need to make to the database here
- */
 async function up () {
-  // Write migration here
   await userModel.updateMany({}, {
     $rename: { name: 'employeId' }
   }, { multi: true })
 }
 
-/**
- * Make any changes that UNDO the up function side effects here (if possible)
- */
 async function down () {
-  // Write migration here
 }
 
 module.exports = { up, down }
